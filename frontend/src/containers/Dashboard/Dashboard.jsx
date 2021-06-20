80445f46e0155f7e264a202f7a1ffbd69e113f57
Basel H. Ashour <basel_h_ashour@hotmail.com>
2021-06-20 23:03:39 +0400

    revised

--- frontend/src/containers/Dashboard/Dashboard.jsx
+++ frontend/src/containers/Dashboard/Dashboard.jsx
@@ -60,7 +60,7 @@ const Dashboard = (props) => {
                 console.log(error)
                 setAllowView(false)
             });
-    }, []);
+    });
 
     return (
         allowView ? (
